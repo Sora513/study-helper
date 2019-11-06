986f27d512cbd40f9ca5e4a15c1a122f0aa9b20d
sora513 <kokubo.tech@gmail.com>
2019-11-06 21:25:23 +0900

    add YYYY/MM/DD

--- js/clock.js
+++ js/clock.js
@@ -8,10 +8,15 @@
         }
         function showClock2() {
             var nowTime = new Date();
+            var nowYear = nowTime.getFullYear();
+            var nowMonth = set2fig(parseInt(nowTime.getMonth())+1);
+            var nowDate = set2fig(nowTime.getDate());
             var nowHour = set2fig(nowTime.getHours());
             var nowMin = set2fig(nowTime.getMinutes());
             var nowSec = set2fig(nowTime.getSeconds());
             var msg = nowHour + ":" + nowMin + ":" + nowSec;
             document.getElementById("RTC").innerHTML = msg;
+            var msg2 = nowYear + "/" + nowMonth + "/" + nowDate;
+            document.getElementById("date").innerHTML = msg2;
         }
         setInterval('showClock2()', 1000);
